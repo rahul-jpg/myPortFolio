74e1da25fbcfe163a2a292b2d60cb656b8106041
Rahul Sarkar <8199829rs@gmail.com>
2023-04-03 10:20:05 +0530

    final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,10 +48,10 @@ const NavBar = ({ isOpen, setIsOpen }) => {
             className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-6"
         >
 
-            <button className="flex-col justify-center items-center hidden lg:flex select-none " onClick={handleClick}>
-                <span className={`bg-dark dark:bg-light block transition-all select-none duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"}`}></span>
-                <span className={`bg-dark dark:bg-light block transition-all select-none duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? "opacity-0" : "opacity-100"}`}></span>
-                <span className={`bg-dark dark:bg-light block transition-all select-none duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"}`}></span>
+            <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"}`}></span>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? "opacity-0" : "opacity-100"}`}></span>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"}`}></span>
             </button>
 
             <div className="w-full justify-between flex items-center lg:hidden">
